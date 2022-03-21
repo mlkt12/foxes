f01451944c78f1037ae5e02596761fe7d84c38bc
nick.koles12.3@gmail.com <nick.koles12.3@gmail.com>
2022-03-21 17:15:25 +0500

    fix links

--- sw.js
+++ sw.js
@@ -1,14 +1,14 @@
 self.addEventListener('install', (e) => {
   e.waitUntil(
     caches.open('fox-store').then((cache) => cache.addAll([
-      '/pwa-examples/a2hs/',
-      '/pwa-examples/a2hs/index.html',
-      '/pwa-examples/a2hs/index.js',
-      '/pwa-examples/a2hs/style.css',
-      '/pwa-examples/a2hs/images/fox1.jpg',
-      '/pwa-examples/a2hs/images/fox2.jpg',
-      '/pwa-examples/a2hs/images/fox3.jpg',
-      '/pwa-examples/a2hs/images/fox4.jpg',
+      '/foxes/',
+      '/foxes/index.html',
+      '/foxes/index.js',
+      '/foxes/style.css',
+      '/foxes/images/fox1.jpg',
+      '/foxes/images/fox2.jpg',
+      '/foxes/images/fox3.jpg',
+      '/foxes/images/fox4.jpg',
     ])),
   );
 });
